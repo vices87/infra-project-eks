b349032e00eb46404634000bfe36af5e74edbcd2
Vicente <vices87@gmail.com>
2026-03-06 00:18:32 -0300

    add metrics-server

--- system-a/dev/services/prometheus/data.tf
+++ system-a/dev/services/prometheus/data.tf
@@ -1,7 +1,7 @@
 data "aws_eks_cluster" "this" {
-  name = dependency.eks.outputs.cluster_name
+  name = var.cluster_name
 }
 
 data "aws_eks_cluster_auth" "this" {
-  name = dependency.eks.outputs.cluster_name
+  name = var.cluster_name
 }
