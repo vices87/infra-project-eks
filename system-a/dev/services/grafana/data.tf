205d017be46ea8036e536dce37eec846066657ca
Vicente <vices87@gmail.com>
2026-03-05 21:20:15 -0300

    dependency fetch cluster

--- system-a/dev/services/grafana/data.tf
+++ system-a/dev/services/grafana/data.tf
@@ -1,9 +1,9 @@
 data "aws_eks_cluster" "this" {
-  name = var.cluster_name
+  name = dependency.eks.outputs.cluster_name
 }
 
 data "aws_eks_cluster_auth" "this" {
-  name = var.cluster_name
+  name = dependency.eks.outputs.cluster_name
 }
 
 data "aws_secretsmanager_secret_version" "grafana" {
