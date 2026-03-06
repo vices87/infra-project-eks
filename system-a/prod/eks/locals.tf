8edf5389ee4da8802bb49fd77ab1b3b21c561bcb
Vicente <vices87@gmail.com>
2026-03-05 21:21:56 -0300

    remove stage prod

--- system-a/prod/eks/locals.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  autoscaler_tags = var.enable_cluster_autoscaler ? {
-    "k8s.io/cluster-autoscaler/enabled"        = "true"
-    "k8s.io/cluster-autoscaler/${var.name}"    = "owned"
-  } : {}
-
-}
--- system-a/stage/services/prometheus/data.tf
+++
@@ -1,7 +0,0 @@
-data "aws_eks_cluster" "this" {
-  name = var.cluster_name
-}
-
-data "aws_eks_cluster_auth" "this" {
-  name = var.cluster_name
-}
