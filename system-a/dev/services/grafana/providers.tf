4660c8e634e0726f619eb360a1d61833d91f2b06
Vicente <vices87@gmail.com>
2026-03-06 00:21:44 -0300

    add metrics-server

--- system-a/dev/services/grafana/providers.tf
+++ system-a/dev/services/grafana/providers.tf
@@ -1,7 +1,7 @@
 provider "helm" {
   kubernetes = {
-    host                   = dependency.eks.outputs.cluster_endpoint
-    cluster_ca_certificate = base64decode(dependency.eks.outputs.cluster_certificate_authority_data)
+    host                   = data.aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.this.token
   }
 }
